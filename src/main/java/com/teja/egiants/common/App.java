6261e28f1701aaee91c42701e82d476b84544a29
bunnymohan <mohansai.ijjada@gmail.com>
2017-09-01 02:57:05 -0500

    First commit

---
+++ src/main/java/com/teja/egiants/common/App.java
@@ -0,0 +1,21 @@
+package com.teja.egiants.common;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.teja.egiants.customer.services.CustomerService;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	ConfigurableApplicationContext context = 
+    		new ClassPathXmlApplicationContext(new String[] {"Spring-Customer.xml"});
+	
+    	CustomerService cust = (CustomerService)context.getBean("customerService");
+    	
+    	System.out.println(cust);
+    	
+    	context.close();
+    }
+}
